427f606a88c96e9e74f7ebdf2a34c3c3789217b1
anddtran <anddtran@gmail.com>
2023-10-24 16:39:30 -0500

    name = main

--- inclass.py
+++ inclass.py
@@ -1,9 +1,5 @@
 import pandas as pd
 
-#access dataframe using dataframe[column_name] and make a list of column 0
-data = pd.read_excel('bag_of_words.xlsx')
-rwords = list(data['RWORDS']) 
-
 #reverse function 
 def reverse(x): 
     reversed_word = x[::-1]
@@ -14,17 +10,22 @@ def find_frequency(data, y):
     frequency = data[data['RWORDS'] == y]['RFREQ']
     return frequency.values[0] #return the value only
 
-unique_words = list(set(rwords))
-reverse_words = [reverse(word) for word in unique_words]
-sorted_words = sorted(reverse_words)
-rhyme_words = [reverse(word) for word in sorted_words]
-frequency_reorder = [find_frequency(data, x) for x in rhyme_words]
+if __name__ == '__main__':
+    #access dataframe using dataframe[column_name] and make a list of column 0
+    data = pd.read_excel('bag_of_words.xlsx')
+    rwords = list(data['RWORDS']) 
+    unique_words = list(set(rwords))
 
-dataframe = pd.DataFrame() #create empty dataframe
-dataframe['rhyme_words'] = rhyme_words #dataframe column 0
-dataframe['frequency_reorder'] = frequency_reorder #dataframe column 1
+    reverse_words = [reverse(word) for word in unique_words]
+    sorted_words = sorted(reverse_words)
+    rhyme_words = [reverse(word) for word in sorted_words]
+    frequency_reorder = [find_frequency(data, x) for x in rhyme_words]
 
-#outputs to xlsx file without the index column from the dataframe
-file_path = './output.xlsx'
-dataframe.to_excel(file_path, index=False, engine='openpyxl') 
-print(dataframe)
+    dataframe = pd.DataFrame() #create empty dataframe
+    dataframe['rhyme_words'] = rhyme_words #dataframe column 0
+    dataframe['frequency_reorder'] = frequency_reorder #dataframe column 1
+
+    #outputs to xlsx file without the index column from the dataframe
+    file_path = './output.xlsx'
+    dataframe.to_excel(file_path, index=False, engine='openpyxl') 
+    print(dataframe)
