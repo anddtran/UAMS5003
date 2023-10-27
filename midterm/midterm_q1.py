ada157c9ccb1cd19ba4ca8f6ff4b4407e7786fc0
anddtran <anddtran@gmail.com>
2023-10-26 22:42:45 -0500

    some midterm work

--- midterm/midterm_q1.py
+++ midterm/midterm_q1.py
@@ -13,13 +13,67 @@
     (d) The program will ﬁnally generate a ﬁle named sequences CG.txt similar to the sequences.txt but have CG’s
     underscored (i.e., if the original ﬁle has ACGT the new ﬁle will have ACGT instead) '''
 
-import regex
+import regex as re
 import os
 
-file_path = '/Users/andrewtran/repos/5003/midterm/sq.txt'
+file_path = 'C:\\Users\\anddt\\OneDrive\\Documents\\GitHub\\UAMS5003\\midterm\\sq.txt'
+sequences = []
 
 if os.path.exists(file_path):
     with open(file_path, 'r') as f:
-        sequences = f.read()
-        print("Length of sequences:", len(sequences)) #check if the file was accessed correctly 
+        for line in f:
+            sequences.append(line.strip())
+        print("Number of lines in sequences:", len(sequences)) #check if the file was accessed correctly
+
+if os.path.exists(file_path):
+    with open(file_path, 'r') as file:
+        sequence = file.read()
+
+dotmatches = re.findall(r'\.+', sequence)
+cgmatches = re.findall(r'CG', sequence)
+
+n_list = []
+for line in sequences:
+    matches = re.findall(r"N+", line)
+    match_count = len(matches)
+    n_list.append(match_count)
+print(f'Number of N+ matches per line: {n_list}')
+
+
+for i in range(1, len(max(dotmatches))+1):
+    y = dotmatches.count(i*'.')
+    x = i * '.'
+    print(f'There are {y} number of {x}')
+
+print(f'There are {len(cgmatches)} CG matches')
+
+
+
+#tried some things here, may go back to it later
+'''x1 = 0
+for i in period_list:
+    #match = re.match(r'^\.$', i)
+    if i == '.':
+    #if re.match(r'^\.$', i):
+        x1 += 1
+#x1 = [re.match('\.', i) for i in period_list]
+x_1 = period_list.count('.')
+print(x_1)'''
+
+'''period_list =[]
+for line in sequence:
+    matches = re.findall(r'\.+', line)
+    period_list.extend(matches)
+
+#print(period_list)
+#print(len(period_list))
+#print(max(period_list))
+#print(len(max(period_list)))'''
+
+'''cgmatches = []
+for line in sequences:
+    cgmatch = re.findall(r'CG', line)
+    cgmatches.extend(cgmatch)
+print(f'There are {len(cgmatches)} CG matches')'''
+
 
